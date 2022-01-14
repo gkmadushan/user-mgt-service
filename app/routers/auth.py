f1ebb155fafc465fa5f4ce500ee98138c9531b28
Kasun <kasun.chathuranga@edgeassociates.se>
2022-01-14 15:07:41 +0530

    sonar

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -15,13 +15,14 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
+
 @router.post("/oauth/token")
-async def login(username = Form(...), password = Form(...), otp : Optional[str] = Form(None), db: Session = Depends(get_db), response: Response = None):
+async def login(username=Form(...), password=Form(...), otp: Optional[str] = Form(None), db: Session = Depends(get_db), response: Response = None):
     access_token = None
     refresh_token = None
     hashed_password = hash(password)
-    #KasunNote: checking the username and password both in the database query is safer than checking them seperately
-    #KasunNote: why? because of if someone is estimating the time then they can use timing to detect the user exists scenario
+    # KasunNote: checking the username and password both in the database query is safer than checking them seperately
+    # KasunNote: why? because of if someone is estimating the time then they can use timing to detect the user exists scenario
     user = db.query(User).filter(User.email == username).filter(User.password == hashed_password).first()
 
     if not user:
@@ -35,7 +36,8 @@ async def login(username = Form(...), password = Form(...), otp : Optional[str] 
 
     response.set_cookie(key="refresh_token", value=refresh_token, httponly=True, secure=True)
     response.set_cookie(key="access_token", value=access_token, httponly=True, secure=True)
-    return {"access_token": access_token, "refresh_token":refresh_token, "token_type": "bearer", "name":user.name, "role":user.role.code, "groups":user.groups}
+    return {"access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer", "name": user.name, "role": user.role.code, "groups": user.groups}
+
 
 @router.post("/oauth/refresh-token")
 async def refresh_token(refresh_token: Optional[str] = Cookie(None), db: Session = Depends(get_db), response: Response = None):
@@ -47,6 +49,7 @@ async def refresh_token(refresh_token: Optional[str] = Cookie(None), db: Session
         response.set_cookie(key="refresh_token", value=refresh_token, httponly=True, secure=True)
         return {"access_token": access_token, "token_type": "bearer"}
 
+
 @router.get("/users/{id}/verify/{token}")
 def confirm(id: str, token, db: Session = Depends(get_db)):
     filters = {}
@@ -56,18 +59,18 @@ def confirm(id: str, token, db: Session = Depends(get_db)):
     try:
         valid_token_count = db.query(OnetimeToken).filter_by(**filters).count()
         if valid_token_count > 0:
-            user = db.query(User).get(id)      
-            
+            user = db.query(User).get(id)
+
             qr = qrcode.make("otpauth://totp/SecOps:OTP?secret={}&issuer=SecOpsRobot".format(user.secret))
             buffer = BytesIO()
             qr.save(buffer, format="PNG")
             image = base64.b64encode(buffer.getvalue()).decode()
             return image
         else:
-            raise HTTPException(status_code=500, detail="Invalid User ID or Token") 
+            raise HTTPException(status_code=500, detail="Invalid User ID or Token")
     except:
         raise HTTPException(status_code=500, detail="Invalid User ID or Token")
-    
+
 
 @router.patch("/users/{id}/verify/{token}")
 def confirm(id: str, token, details: ConfirmUser,  db: Session = Depends(get_db)):
@@ -78,7 +81,7 @@ def confirm(id: str, token, details: ConfirmUser,  db: Session = Depends(get_db)
     try:
         valid_token_count = db.query(OnetimeToken).filter_by(**filters).count()
         if valid_token_count >= 0:
-            user = db.query(User).get(id)    
+            user = db.query(User).get(id)
             if (pyotp.TOTP(user.secret).now() == details.otp):
                 active_token = db.query(OnetimeToken).filter_by(**filters)[0]
                 user.active = 1
@@ -87,9 +90,8 @@ def confirm(id: str, token, details: ConfirmUser,  db: Session = Depends(get_db)
                 db.add(user)
                 db.add(active_token)
                 db.commit()
-                return {"user_id":user.id}
+                return {"user_id": user.id}
             else:
                 raise HTTPException(status_code=500, detail="Invalid OTP "+pyotp.TOTP(user.secret).now())
     except:
         raise HTTPException(status_code=500, detail="Invalid OTP "+pyotp.TOTP(user.secret).now())
-    
